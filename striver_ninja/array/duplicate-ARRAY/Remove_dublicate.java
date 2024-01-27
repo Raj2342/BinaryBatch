7f86c0b1311786ccbf99c181f65de39a5673d7b0
Himanshu Gupta <technologys411@gmail.com>
2024-01-27 15:10:06 +0530

    hii

--- striver_ninja/array/duplicate-ARRAY/Remove_dublicate.java
+++ striver_ninja/array/duplicate-ARRAY/Remove_dublicate.java
@@ -32,3 +32,32 @@ public class Remove_dublicate {
             
        }
 }
+
+
+
+
+
+//   approach-2   - smart programming 
+// public class Approach_second {
+        
+//        public static void main(String[] args) {
+              
+//               int arr[] = { 1,2,2,3,3,3,4,5,5};
+//               int[]  temp = new int[arr.length];
+//               int j=0;
+//               for(int i=0; i<arr.length-1; i++){
+//                if (arr[i]!=arr[i+1]) {
+//                        temp[j]= arr[i];
+//                        j++;
+//                }
+
+//               }
+//               temp[j]= arr[arr.length-1];
+//               // print 
+//               for(int i=0; i<arr.length;i++){
+//                   System.out.print(temp[i]+ " ");
+//               }
+
+//        }
+// }  
+
