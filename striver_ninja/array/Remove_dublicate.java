432928c11948bc78e847a1bbfd05b7c9d5855366
Himanshu Gupta <technologys411@gmail.com>
2024-01-27 14:36:40 +0530

    duplicate array

--- striver_ninja/array/Remove_dublicate.java
+++ striver_ninja/array/Remove_dublicate.java
@@ -2,14 +2,33 @@ import java.util.Arrays;
 
 public class Remove_dublicate {
         
-         public static void sort(int arr[]){
-         
-              Arrays.sort(arr);
+         public static int  play(int arr[] ){
+          int[] temp = new int[arr.length];
+          int  j=0;
+          for(int i=0; i<arr.length-1;i++){
+                      
+                    if (arr[i]!=arr[i+1]) {
+                          temp[j] = arr[i];
+                          j++;
+                         }
+               
+                         temp[j] = arr[arr.length-1];
+
+               
+            }
+          //   System.out.println(Arrays.toString(temp));
+          return  temp[temp.length-3];
+              
          }
     
     
     
     public static void main(String[] args) {
-           int arr[] = {1,1,2,2,2,3,3};
+           int arr[] = {1,2,2,3,4,5,5};
+          
+           System.out.println(play(arr));
+          
+           
+            
        }
 }
