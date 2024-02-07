35d529ad2445c094ce4d2503c78c9f446797be5e
Himanshu Gupta <technologys411@gmail.com>
2024-02-07 13:50:45 +0530

    overloading

---
+++ oops/instance variable/method_overloading.java
@@ -0,0 +1,8 @@
+/*
+  main method me call karte time infinte parameter assign  kar sakte ho  but dusre method ke 
+  parameter different hone chaiye 
+  
+ */
+public class method_overloading {
+    
+}
