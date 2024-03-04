0f68d972ad6ddc5744b924474449fcbac49834a8
Himanshu Gupta <technologys411@gmail.com>
2024-03-04 21:56:48 +0530

    odd_even

--- striver_ninja/array/odd_even_print/odd_even_print.java
+++ striver_ninja/array/odd_even_print/odd_even_print.java
@@ -8,10 +8,8 @@ class   odd_even_print {
             int a[] =  {8,5,10,12,3,1,4};
               ArrayList<Integer> s1 = new ArrayList<>();
               ArrayList<Integer> s2 = new ArrayList<>();
-              //for sum odd and event no.
-              
-
-              for(int i=0; i<a.length;i++){
+            //    print  seperate even and odd  array 
+                for(int i=0; i<a.length;i++){
                  if (a[i]%2==0) {
                       s1.add(a[i]);
                       
@@ -26,9 +24,28 @@ class   odd_even_print {
              System.out.println(s2);
              System.out.println(s2.size());
 
-             // add even array and odd aray 
+             // sum of  even array and odd aray  using for each loop
 
-            //  int evenSum = sumArray(s1); 
+             int SumEven =0;
+             int SumOdd =0;
+
+             for(int even : s1){
+                  SumEven = SumEven + even;
+             }
+             
+             for(int odd : s2){
+                   SumOdd = SumOdd + odd;
+                  }
+                  
+                
+                  System.out.println( "sum of even: "+SumEven);
+                  System.out.println("sum of odd : "+SumOdd);
+                  // how many element  of even
+                  System.out.println(s1.size());
+                   // how many element  of odd
+                  System.out.println(s2.size());
+
+           
 
 
 
---
+++ striver_ninja/array/odd_even_print/rough.java
@@ -0,0 +1,11 @@
+public class rough {
+       
+    public static void main(String[] args) {
+           int arr[] = { 2,3,4,5,6};
+           for(int a : arr){
+              System.out.println(a);
+           }
+           
+
+    }
+}
