e899bcf6f0b3806c13c748c018f3e86caa11820c
Himanshu Gupta <technologys411@gmail.com>
2024-03-06 21:47:11 +0530

    constructor

--- striver_ninja/array/odd_even_print/rough.java
+++ striver_ninja/array/odd_even_print/rough.java
@@ -2,10 +2,12 @@ public class rough {
        
     public static void main(String[] args) {
            int arr[] = { 2,3,4,5,6};
-           for(int a : arr){
-              System.out.println(a);
-           }
-           
+           int sum =0;
+           for(int i=0; i<arr.length;i++){
+              sum = sum + arr[i];
+            }
+            System.out.println( sum);
+
 
     }
 }
